4ddbf03e1fd58d275c013d8db6a0ca66b2632d27
moreal <dogeonlove0326@gmail.com>
2017-04-22 22:04:13 +0900

    스택 구현 - 자료구조
    스택에서 top이 -1에서 시작한다는 것에 맞춰서 수정했습니다.

--- Stack.c
+++ Stack.c
@@ -9,7 +9,7 @@
 //9999 = pop() , 숫자 입력시 push() , 8888 = 종료 , 오버플로우, 언더플로우
 
 int stack[MAX]; // 스택 배열
-int top = 0; // 스택 top 변수
+int top = -1; // 스택 top 변수
 int input; //사용자가 원하는 수. 입력하는 수
 
 void setcolor(int a) // 색깔로 꾸며보고 싶어서 만든 필요없는 함수 입니다.
@@ -19,13 +19,13 @@ void setcolor(int a) // 색깔로 꾸며보고 싶어서 만든 필요없는 함수 입니다.
 
 char check_flows() //오버플로우 여부를 확인 합니다.
 {
-	if (top < 0)
+	if (top < -1)
 	{
 		top++;
 		setcolor(12); printf("[ERROR]"); setcolor(11); printf(" Underflow 발생.\a\n\n"); setcolor(15);
 		return 1;
 	}
-	else if(top > MAX)
+	else if (top >= MAX)
 	{
 		top--;
 		setcolor(12); printf("[ERROR]"); setcolor(11); printf(" Overflow 발생.\a\n\n"); setcolor(15);
@@ -35,23 +35,23 @@ char check_flows() //오버플로우 여부를 확인 합니다.
 
 void push() //스택에 푸시합니다.
 {
-	top++;
+	++top;
 	if (check_flows() != 1)
 	{
-		stack[top - 1] = input;
-		setcolor(11); printf(" %d", stack[top - 1]);
+		stack[top] = input;
+		setcolor(11); printf(" %d", stack[top]);
 		setcolor(15); printf("을 푸시했습니다.\n\n");
 	}
 }
 
 int pop() //스택에서 팝합니다.
 {
-	top--;
+	--top;
 	if (check_flows() != 1)
 	{
-		setcolor(11); printf(" %d", stack[top]);
-		setcolor(15); printf("을 팝했습니다\n\n", stack[top]);
-		stack[top] = input;
+		setcolor(11); printf(" %d", stack[top+1]);
+		setcolor(15); printf("을 팝했습니다\n\n", stack[top+1]);
+		stack[top+1] = input;
 	}
 }
 
@@ -70,16 +70,16 @@ int main() // 메인함수
 		printf("------------------------------\n\n");
 		printf(" 최대 저장 가능한 양 : %d\n", MAX);
 		printf("\n 입력 : ");
-		
+
 		scanf_s("%d", &input);
 		switch (input)
 		{
-			case POP: pop(); break;
-			case EXIT: system("cls"); system("mode con: lines=1 cols=42"); setcolor(11); printf("안녕히 가세요 종료하려면 아무키나 누르세요"); _getch(); return 0; break;
-			default: push(); break;
+		case POP: pop(); break;
+		case EXIT: system("cls"); system("mode con: lines=1 cols=42"); setcolor(11); printf("안녕히 가세요 종료하려면 아무키나 누르세요"); _getch(); return 0; break;
+		default: push(); break;
 		}
 
-		printf(" 현재 저장되있는 양 : %d\n [ ", top);
-		for (int i = 0; i < top; i++) printf("%d ", stack[i]); printf("]\n\n");
+		printf(" 현재 저장되있는 양 : %d\n [ ", top+1);
+		for (int i = 0; i < top+1; i++) printf("%d ", stack[i]); printf("]\n\n");
 	}
 }
