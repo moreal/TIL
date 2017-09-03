4dc0b4ab463d3f9ca4fa8574aa8d2c181c63f13e
LDG <dogeonlove0326@gmail.com>
2017-09-03 19:08:20 +0900

    003. Operator

---
+++ 003. Operator/src/com/moreal/main/Main.java
@@ -0,0 +1,13 @@
+package com.moreal.main;
+
+public class Main {
+	public static void main(String args[])
+	{
+		int iTemp = 0;
+		System.out.println("iTemp++ = " + iTemp++);
+		System.out.println("iTemp-- = " + iTemp--);
+		System.out.println("--iTemp = " + --iTemp);
+		System.out.println("++iTemp = " + ++iTemp);
+		
+	}
+}
