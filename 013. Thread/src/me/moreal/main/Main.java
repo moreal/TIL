69f83871a382f50d575d788c6f6b0d63f5eeb33e
LDG <dogeonlove0326@gmail.com>
2017-10-20 17:29:11 +0900

    Not perfect

---
+++ 013. Thread/src/me/moreal/main/Main.java
@@ -0,0 +1,16 @@
+package me.moreal.main;
+
+public class Main {
+	/*
+	 * 기본적으로 프로그램은 절차적인 개념을 지닌다.
+	 * 코드가 짜여져 있는 순서대로 실행되는 것이다.
+	 * 허나 이를 극복 하는 것이 있다.
+	 * 
+	 * Thread라는 것이 C언어에 있었다.
+	 * 스레드는 같은 프로세스내에서 같은 자원을 사용하여 동시 실행한다.
+	 * 
+	 * Thread 참고 사항 (	https://ko.wikipedia.org/wiki/POSIX , https://ko.wikipedia.org/wiki/유닉스_신호  )
+	 * 
+	 * 
+	 */
+}
