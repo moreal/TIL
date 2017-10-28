8ecd9665b0a0dfbeb79289751ec12768c04667e1
Moreal <dogeonlove0326@gmail.com>
2017-10-28 15:56:44 +0900

    TIL clear end

--- Security/WarGame/Code/Python/webhacking.kr/webhacking.kr-33-10.py
+++
@@ -1,6 +0,0 @@
-string='119.207.125.184'
-
-for i in range(0, 9):
-    string.replace(i,ord(i))
-
-print(string)
--- Security/WarGame/Code/Python/webhacking.kr/webhacking.kr-33-4.py
+++
@@ -1,6 +0,0 @@
-import urllib2
-
-url = 'http://webhacking.kr/challenge/bonus/bonus-6/l4.php'
-
-connection = urllib2.Request(url)
-connection.add_header('Cookie','PHPSESSID=2bf8d5d0862cc38166a5452c0910f556')
