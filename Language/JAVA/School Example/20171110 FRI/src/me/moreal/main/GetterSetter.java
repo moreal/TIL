5f00269d4a9fd8372a7383ead1715888d69c4f35
Moreal <dogeonlove0326@gmail.com>
2017-11-10 22:55:53 +0900

    School Lesson

---
+++ Language/JAVA/School Example/20171110 FRI/src/me/moreal/main/GetterSetter.java
@@ -0,0 +1,7 @@
+package me.moreal.main;
+
+public class GetterSetter {
+	private int a;
+	int getA() { return a; }
+	void setA(int value) { a = value; }
+}
---
+++ Language/JAVA/School Example/20171110 FRI/src/me/moreal/main/Main.java
@@ -0,0 +1,9 @@
+package me.moreal.main;
+
+public class Main {
+	public static void main(String args[]){
+		GetterSetter First = new GetterSetter();
+		First.setA(1000);
+		First.getA();
+	}
+}
